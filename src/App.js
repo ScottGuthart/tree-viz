b8c283e6948944cb45ef11503b77ebd1c520d7ef
Scott Guthart <seguthart@gmail.com>
2021-10-13 16:22:29 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import UploadButton from "./UploadButton";
+import RichObjectTreeView from "./RichObjectTreeView";
+
+function App() {
+  const [tree, setTree] = React.useState(null);
+
+  const uploadTree = (ev) => {
+    document.body.style.cursor = "progress";
+    const file = ev.target.files[0];
+    if (file) {
+      console.info(ev.target.files[0]);
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const uploadedTree = JSON.parse(event.target.result);
+        console.log(uploadedTree);
+        ev.target.value = "";
+        setTree(uploadedTree);
+        document.body.style.cursor = "default";
+      };
+      reader.onerror = (error) => {
+        alert(error);
+        ev.target.value = "";
+        document.body.style.cursor = "default";
+      };
+
+      console.info("Reading JSON");
+      reader.readAsText(file);
+    }
+  };
+  return !tree ? (
+    <UploadButton
+      onChange={uploadTree}
+      id={"uploadTree"}
+      title={"Upload Tree"}
+      accept={".json"}
+    />
+  ) : (
+    <RichObjectTreeView data={tree} />
+  );
+}
+
+export default App;
---
+++ src/UploadButton.js
@@ -0,0 +1,30 @@
+// import { CloudUpload } from "@material-ui/icons";
+
+import { Button } from "@mui/material";
+
+export default function UploadButton({
+  id,
+  onChange,
+  title = "Upload",
+  color = "primary",
+  accept = ".xlsx",
+  variant = "text",
+  size = "small",
+}) {
+  return (
+    <div>
+      <input
+        accept={accept}
+        style={{ display: "none" }}
+        id={id}
+        type="file"
+        onChange={onChange}
+      />
+      <label htmlFor={id}>
+        <Button variant={variant} color={color} component="span" size={size}>
+          {title}
+        </Button>
+      </label>
+    </div>
+  );
+}
