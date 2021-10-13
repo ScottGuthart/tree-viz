7a59bfd570af2b4694201bd06eb00c31ee4238a0
Scott Guthart <seguthart@gmail.com>
2021-10-13 16:23:39 -0400

    removed icons

--- src/RichObjectTreeView.js
+++ src/RichObjectTreeView.js
@@ -1,7 +1,5 @@
 import * as React from "react";
 import TreeView from "@mui/lab/TreeView";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import TreeItem, { treeItemClasses } from "@mui/lab/TreeItem";
 import { alpha, Container, styled, SvgIcon } from "@mui/material";
 import Collapse from "@mui/material/Collapse";
